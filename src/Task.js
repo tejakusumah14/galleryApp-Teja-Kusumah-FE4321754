5eae6b3cdf856b1123e4afd669ffd462f6697035
tejakusumah14 <kusumahteja6@gmail.com>
2023-06-17 14:43:08 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Teja Kusumah"; // TODO: replace this
+const studentId = "FE4321754"; // TODO: replace this
+
+export { url, studentName, studentId };
